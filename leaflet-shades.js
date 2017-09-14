26f66b90ef19de3a8c4088bb7b9c2dca55186f9d
Mandy <mandy@mapzen.com>
2017-09-14 16:09:16 -0400

    added semicolons; edited constructor

--- leaflet-shades.js
+++ leaflet-shades.js
@@ -1,45 +1,49 @@
 L.LeafletShades = L.Layer.extend({
-	onAdd: function(map) {
-		var pane = map.getPane(this.options.pane)
+	options: { 
+		shades = true; 
+	},
+
+	initialize: function(rect, options) {
+		_createShades(rect);
 	}
 
 	_createShades: function(rect) {
-	  const regionSelector = map._panes.overlayPane
-	  map._shadeContainer = L.DomUtil.create('div', 'leaflet-areaselect-container', regionSelector)
-	  map._topShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer)
-	  map._bottomShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer)
-	  map._leftShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer)
-	  map._rightShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer)
-	  updateShades(rect)
-	}
+	  const regionSelector = map._panes.overlayPane;
+	  map._shadeContainer = L.DomUtil.create('div', 'leaflet-areaselect-container', regionSelector);
+	  map._topShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer);
+	  map._bottomShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer);
+	  map._leftShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer);
+	  map._rightShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer);
+	  updateShades(rect);
+	},
 
 	_setDimensions: function(element, dimension) {
-	  element.style.width = dimension.width + 'px'
-	  element.style.height = dimension.height + 'px'
-	  element.style.top = dimension.top + 'px'
-	  element.style.left = dimension.left + 'px'
-	}
+	  element.style.width = dimension.width + 'px';
+	  element.style.height = dimension.height + 'px';
+	  element.style.top = dimension.top + 'px';
+	  element.style.left = dimension.left + 'px';
+	},
 
 	_getOffset: function(northEastPoint, southWestPoint) {
 	  // Getting the transformation value through style attributes
-	  let transformation = map.getPanes().mapPane.style.transform
-	  const startIndex = transformation.indexOf('(')
-	  const endIndex = transformation.indexOf(')')
-	  transformation = transformation.substring(startIndex + 1, endIndex).split(',')
+	  let transformation = map.getPanes().mapPane.style.transform;
+	  const startIndex = transformation.indexOf('(');
+	  const endIndex = transformation.indexOf(')');
+	  transformation = transformation.substring(startIndex + 1, endIndex).split(',');
 	  const offset = {
 	    x: Number(transformation[0].slice(0, -2) * -1),
 	    y: Number(transformation[1].slice(0, -2) * -1)
 	  }
 	  return offset
-	}
+	},
 
 	_updateShades: function(rect) {
-	  const rectBounds = rect.getBounds()
-	  const size = map.getSize()
-	  const offset = getOffset()
+	  const rectBounds = rect.getBounds();
+	  const size = map.getSize();
+	  const offset = getOffset();
 
-	  const northEastPoint = map.latLngToContainerPoint(rectBounds.getNorthEast())
-	  const southWestPoint = map.latLngToContainerPoint(rectBounds.getSouthWest())
+	  const northEastPoint = map.latLngToContainerPoint(rectBounds.getNorthEast());
+	  const southWestPoint = map.latLngToContainerPoint(rectBounds.getSouthWest());
 
 	  setDimensions(map._topShade, {
 	    width: size.x,
