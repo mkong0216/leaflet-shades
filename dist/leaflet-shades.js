75cf2316e864c98de0e40d3cbda12fa2db1d6784
Mandy <mandy@mapzen.com>
2017-10-20 11:13:24 -0400

    removed commas

--- dist/leaflet-shades.js
+++ dist/leaflet-shades.js
@@ -2147,7 +2147,7 @@ var LeafletShades = L.Layer.extend({
 	_onBoundsChanged: function (event) {
 		var _bounds = event.layer.getBounds();
 		this.fire('shades:bounds-changed', {
-			bounds: _bounds,
+			bounds: _bounds
 		});
 		this._updateShades(_bounds);
 	}, 
@@ -2155,7 +2155,7 @@ var LeafletShades = L.Layer.extend({
 	_updatedMapPosition: function(event) {
 		if (this._bounds) {
 			this.fire('shades:bounds-changed', {
-				bounds: this._bounds,
+				bounds: this._bounds
 			});
 			this._updateShades(this._bounds);
 		}
